87f679c881460166778007e2a613a7fd53a78a66
zzidun-pavo <zzidun@qq.com>
2020-04-06 17:58:50 +0800

    Signed-off-by: zzidun-pavo <zzidun@qq.com>

---
+++ Calcul/calcul.cpp
@@ -0,0 +1,11 @@
+#include "calcul.h"
+
+int calcul(int range, int num)
+{
+	FILE *oFile;
+	oFile = fopen("./Exercises.txt", "w");
+	for (int i = 1; i <= num; ++i)
+	{
+	
+	}
+}
---
+++ Calcul/calcul.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <random>
+#include <ctime>
+#include <stdio.h>
+
+int calcul(int range, int num);
+
+	
