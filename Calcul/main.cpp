e51592296f16bef64b52e2ea988ed68b85250814
zzidun-pavo <zzidun@qq.com>
2020-04-06 16:51:09 +0800

    主函数

--- Calcul/main.cpp
+++ Calcul/main.cpp
@@ -1,15 +1,31 @@
 ﻿#include "fraction.h"
+#include "getopt.h"
+#include "trans.h"
+#include "calcul.h"
 
 #include <iostream>
 
-int main(int argc, char* argv[])
+int main(int args, char* argv[])
 {
-	if (argc == 1) {
-		puts("使用-r参数指定生成题目的范围");
-		puts("使用-n参数指定生成题目的数量");
-		puts("使用-e <exercisefile> -a <answer>来批改答案");
-	}
-	else {
+	char c;
+	char argStr[] = "r:n:e:a:";
+	int range = 0;
+	int num = 0;
+	char* aFile, * eFile;
+
+	while ((c = getopt(args, argv, argStr)) != -1) {
+		switch (c) {
+		case 'r': range = str2Int(argv[optind-1], 10); break;
+		case 'n': num = str2Int(argv[optind-1], 10); break;
+		case 'e': eFile = argv[optind]; break;
+		case 'a': aFile = argv[optind]; break;
+		}
 
 	}
+	if (!range || !num) {
+		puts("使用-r <range> -n <number> 参数指定生成题目的范围和数量 ");
+		puts("使用-e <exercisefile> -a <answer>来批改答案");
+	}
+
+	return 0;
 }
