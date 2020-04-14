5f7ac66ea701b03139d4128e7329a9d3c1bb521b
zzidun-pavo <zzidun@qq.com>
2020-04-14 20:49:23 +0800

    删除了混乱的旧文件

--- Calcul/calcul.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-//生成题目
-void makeQuestion(int range, int num);
-
-//检查答案
-void checkAnswer(char* ePath, char* aPath);
--- Calcul/trans.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-//将字符串转成整型
-int cstrToInt(const char* str);
